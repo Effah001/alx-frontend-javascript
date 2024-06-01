d770dcf5167c16c11efd0294a5310f9edac41e91
Effah001 <effah210@gmail.com>
2024-06-01 09:49:46 +0000

    update

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,10 +2,14 @@ export default function handleResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({ status: 200, body: 'Success' });
-      console.log('Got a response from the API');
     } else {
       reject(new Error('The fake API is not working currently'));
-      console.log('Got a response from the API');
     }
+  }).then((response) => {
+    console.log('Got a response from the API');
+    return response;
+  }).catch((error) => {
+    console.log('Got a response from the API');
+    throw error;
   });
 }
