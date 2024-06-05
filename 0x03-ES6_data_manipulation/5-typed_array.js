cae38f1631416f9b1c06137db812f91324925d94
Effah001 <effah210@gmail.com>
2024-06-05 01:15:34 +0000

    update

--- 0x03-ES6_data_manipulation/5-typed_array.js
+++
@@ -1,3 +0,0 @@
-const buffer = new ArrayBuffer(length, position, value);
-
-const view = new Uint8Array(buffer);
--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,9 @@
 export default function createArrayBuffer(length, position, value) {
-    const buffer = new ArrayBuffer(length);
-    
-    if (position >= 0 && position < length) {
-        const dataView = new DataView(buffer);
-        dataView.setInt8(position, value);
-    } else {
-        throw new Error('Position outside range');
-    }
-    
-    return buffer;
+  const buffer = new ArrayBuffer(length);
+  const view = new Uint8Array(buffer);
+  if (position >= buffer.byteLength) {
+    throw new Error('Position outside range');
+  }
+  view[position] = value;
+  return new DataView(buffer);
 }
