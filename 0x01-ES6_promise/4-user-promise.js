94b7c94869621d51c2f78f71f648db26b1a450f3
Effah001 <effah210@gmail.com>
2024-06-26 07:10:46 +0000

    user_promise

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,9 @@
+export default function signUpUser(firstName, lastName) {
+	return new Promise((resolve, reject) => {
+		if (firstName && lastName) {
+			resolve({firstName: firstName, lastName: lastName});
+		}else {
+			reject('Invalid name');
+		}
+	});
+}
