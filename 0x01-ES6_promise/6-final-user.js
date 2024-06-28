047eb268f61c59d5aeadc37fe028351ea3b5bc34
Effah001 <effah210@gmail.com>
2024-06-28 10:25:22 +0000

    mutiplepromises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,9 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(filename);
+
+  return Promise.allSettled([userPromise, photoPromise]);
+}
