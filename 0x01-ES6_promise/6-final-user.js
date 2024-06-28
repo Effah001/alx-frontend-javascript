787df83e7b4400ff459d8fb17f9f99e5db57a366
Effah001 <effah210@gmail.com>
2024-06-28 10:55:13 +0000

    uploadprofile

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,11 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
+export default function handleSignupAndUpload(firstName, lastName, filename) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(filename);
 
-  return Promise.allSettled([userPromise, photoPromise])
-    .then((results) => results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return result.value;
-      }
-      return { status: result.status, reason: result.reason.message };
-    }));
+  return Promise.allSettled([userPromise, photoPromise]).then((results) => results.map((result) => (result.status === 'fulfilled'
+    ? result.value
+    : { status: result.status, reason: result.reason.message })));
 }
