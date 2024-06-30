8bc749300a1e857b28f5209dd38db1c4e08f3245
Effah001 <effah210@gmail.com>
2024-06-30 10:09:12 +0000

    building_class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,23 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('sqrt must be a number');
+    }
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Method evacuationWarningMessage must be implemented');
+  }
+}
