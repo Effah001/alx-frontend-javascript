1190943dd7a86fd91ad099dd95f43acdfe9c603f
Effah001 <effah210@gmail.com>
2024-07-02 14:16:51 +0000

    clone ev car

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -4,14 +4,12 @@ export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
 
-    if (typeof range !== 'string') {
-      throw new TypeError('range must be a string');
-    }
 
     this._range = range;
   }
 
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
+    const Clonedclass = this.constructor;
+	return new Clonedclass();
   }
 }
